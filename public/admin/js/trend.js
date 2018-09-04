562f1887c2126387a016ded5d24efb5b0ab3aca5
minhtruongmato <truong.do@matocreative.vn>
2018-09-04 17:51:54 +0700

    [done] update avatar in trend function in admin

--- public/admin/js/trend.js
+++ public/admin/js/trend.js
@@ -15,8 +15,38 @@ window.onload = function(){
                     image : image, id : id, _token : token
                 },
                 success: function(res){
-                    check.parent('div').fadeOut();
+                    if(res.result == false){
+                        alert('Hình ảnh này đang được chọn làm avatar nên không thể xóa!')
+                    }else{
+                        check.parent('div').fadeOut();
+                    }
                     
+                },
+            });
+        }
+        
+    });
+
+    $('.active-avatar').click(function(){
+        var check = $(this);
+        var image = $(this).data('image');
+        var id = $(this).data('id');
+        var token = $('#token').val();
+        if(confirm('Chọn ảnh này làm Avatar')){
+            $.ajax({
+                url: url + '/atelier/admin/trend/activeAvatar',
+                method: 'POST',
+                data: {
+                    image : image, id : id, _token : token
+                },
+                success: function(res){
+                    // check.parent('div').fadeOut();
+                    if(res.result == true){
+                        alert("Thay đổi Avatar thành công!");
+                    }else{
+                        alert("Thay đổi Avatar thất bại!");
+                    }
+                    location.reload();
                 },
             });
         }
